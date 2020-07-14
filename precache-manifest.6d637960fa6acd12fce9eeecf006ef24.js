de0bca304122f717d10292340bb35dcf08df8d45
dorydaniel <dory_daniel@hotmail.com>
2020-07-14 17:22:25 +0300

    Updates

---
+++ precache-manifest.6d637960fa6acd12fce9eeecf006ef24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a56a7ca4da80fd4311d9184019cb17",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "6180a6b0c67dc8bc416a",
+    "url": "/weather/static/css/main.4b3f3965.chunk.css"
+  },
+  {
+    "revision": "8bcf71f4a8453f05a279",
+    "url": "/weather/static/js/2.7a59ba56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.7a59ba56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6180a6b0c67dc8bc416a",
+    "url": "/weather/static/js/main.374737db.chunk.js"
+  },
+  {
+    "revision": "1557ce1959b3418744e2",
+    "url": "/weather/static/js/runtime-main.80a89714.js"
+  }
+]);
